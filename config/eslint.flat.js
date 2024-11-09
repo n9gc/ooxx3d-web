bbf44da128b8935f29435b9604006db05cf60118
Eosellmay Li <2507917746@qq.com>
2024-11-09 21:56:06 +0800

    Initial commit

---
+++ config/eslint.flat.js
@@ -0,0 +1,52 @@
+// @ts-check
+/// <reference types="node" />
+
+import eslint from '@eslint/js';
+import accurtypeStyle from 'eslint-config-accurtype-style';
+import { getDirname } from 'esm-entry';
+import globals from 'globals';
+import path from 'path';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+	...accurtypeStyle,
+	eslint.configs.recommended,
+	...tseslint.configs.stylisticTypeChecked,
+	{
+		name: 'TS Base Config',
+		languageOptions: {
+			parserOptions: {
+				tsconfigRootDir: path.join(getDirname(import.meta.url), '..'),
+				project: [
+					'config/tsconfig.json',
+					'packages/*/tsconfig.json',
+				],
+			},
+		},
+	},
+	{
+		name: 'Opt Rules',
+		rules: { 'no-unused-vars': 'off' },
+	},
+	{
+		name: 'Global Ignore',
+		ignores: [
+			'**/*.md',
+			'eslint.config.mjs',
+			'.*',
+			'**/dist',
+		],
+	},
+	{
+		name: 'JS Ignore',
+		ignores: [
+		],
+	},
+	{
+		name: 'Node Env',
+		files: [
+			'config/**',
+		],
+		languageOptions: { globals: globals.node },
+	},
+);
